f9fc3883772fa313681471cad03a924da415ee45
Fabricio Moura <fabriciomourafilho@gmail.com>
2020-11-25 11:40:33 -0300

    alteração

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main(){
     imprimirMenu();
     int opcao1 = opcoes();
     if(opcao1 == 2){
-        clearScreen();
+        clear();
         resumo();
 
     }
@@ -20,6 +20,8 @@ void clearScreen()
   const char *CLEAR_SCREEN_ANSI = "\e[1;1H\e[2J";
   write(STDOUT_FILENO, CLEAR_SCREEN_ANSI, 12);
 }
+
+
 
 int opcoes(){
     int escolha;
@@ -45,4 +47,14 @@ void imprimirMenu(){
     printf("█░░░▀░░░▄▄▄▄▄░░██░▀▀░█-----------------------------------█░░░▀░░░▄▄▄▄▄░░██░▀▀░█\n");
     printf("─▀▄▄▄▄▄▀─────▀▄▄▄▄▄▄▀                                    ─▀▄▄▄▄▄▀─────▀▄▄▄▄▄▄▀\n\n");
 
+}
+
+void clear(){
+    #if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
+        system("clear");
+    #endif
+
+    #if defined(_WIN32) || defined(_WIN64)
+        system("cls");
+    #endif
 }
