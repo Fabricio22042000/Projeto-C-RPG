496aaef3e6fdf9d91fa67a515b720945eab10c29
Fabricio Moura <fabriciomourafilho@gmail.com>
2020-12-01 22:59:57 -0300

    projeto

---
+++ Projeto-C-RPG-master/main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <gconio.h>
+#include <unistd.h>
+#include <time.h>
+#include </home/fabricio/Documentos/workspace/RPG/Projeto-C-RPG-master/bibliotecasP.c>
+
+int main(){
+    menu();
+    historia();
+}
